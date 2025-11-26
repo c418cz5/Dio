af816b6e2f746779ba2e15f1289c3ea4ee2763b6
Rick <1477624023@qq.com>
2025-11-26 21:58:39 +1030

    333

--- movable_object.py
+++ movable_object.py
@@ -24,9 +24,43 @@ class MovableObject:
 
     
     @property
-     def y(self) -> int:
+    def y(self) -> int:
         return self._y
+    
+
+    @y.setter
+    def y(self, y: int) -> None:
+       self.set_y(y)
+
+    def set_x(self, x: int) -> None:  
+        if isinstance(x, int) and x >= 0:
+            self._x = x
+        else:
+            print("Error: x coordinate must be a non-negative integer")
+
+
         
-      
+    def set_y(self, y: int) -> None:
+        if isinstance(y, int) and y >= 0:
+            self._y = y
+        else:
+            print("Error: y coordinate must be a non-negative integer")
+        
+    def move(self, dx: int, dy: int) -> None:
+        new_x = self._x + dx
+        new_y = self._y + dy
+        if new_x >= 0 and new_y >= 0:
+            self._x = new_x
+            self._y = new_y
+            print("Moved to x={}, y={}".format(new_x, new_y))
+        else:
+            print("Error: Move would result in negative coordinates")
+    
+    def __str__(self) -> str:
+        return "MovableObject(x={}, y={})".format(self._x, self._y)
+
+    def __repr__(self) -> str:
+        return "MovableObject(x={}, y={})".format(self._x, self._y)
+
 
 
